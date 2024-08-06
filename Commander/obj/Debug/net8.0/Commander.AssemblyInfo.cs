f16cc2e870c7c6fcd4d783bb213ba5e526dc1b7d
Agustin Cabrera <58793527+AgustinCabrera@users.noreply.github.com>
2024-08-06 14:27:30 -0300

    process

--- Commander/obj/Debug/net8.0/Commander.AssemblyInfo.cs
+++ Commander/obj/Debug/net8.0/Commander.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Commander")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8313a945d42f067b0b96e553d2a0a0762b20e665")]
 [assembly: System.Reflection.AssemblyProductAttribute("Commander")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Commander")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
